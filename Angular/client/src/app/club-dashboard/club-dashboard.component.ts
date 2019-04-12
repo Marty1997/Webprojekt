3fb5723496706918b53eebeaa278c6cec5efd253
Rune Baun Andersen <1067514@ucn.dk>
2019-04-12 13:48:37 +0200

    More angular optimiziation

---
+++ Angular/client/src/app/club-dashboard/club-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-club-dashboard',
+  templateUrl: './club-dashboard.component.html',
+  styleUrls: ['./club-dashboard.component.css']
+})
+export class ClubDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Angular/client/src/app/front-page/front-page-image/front-page-image.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-front-page-image',
+  templateUrl: './front-page-image.component.html',
+  styleUrls: ['./front-page-image.component.css']
+})
+export class FrontPageImageComponent implements OnInit {
+  modalRef: BsModalRef;
+  
+  constructor(private modalService: BsModalService) { }
+  
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+  
+
+  ngOnInit() {
+  }
+    
+  }
