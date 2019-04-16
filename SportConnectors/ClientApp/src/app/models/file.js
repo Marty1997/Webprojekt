89bb97530bb7ab4cb226340857fb44708ae73027
Morten Albertsen <1067316@ucn.dk>
2019-04-16 11:44:18 +0200

    Clean up

--- SportConnectors/ClientApp/src/app/models/file.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var file = /** @class */ (function () {
-    function file() {
-    }
-    return file;
-}());
-exports.file = file;
-//# sourceMappingURL=file.js.map
