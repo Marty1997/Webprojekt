77adce21484940b7bc72dcb115267e3c34768b6c
Rune Baun Andersen <1067514@ucn.dk>
2019-04-25 14:28:23 +0200

    Training hours optimized

--- client/src/app/front-page/front-page-image/register-club/training-hours-to/training-hours-to.component.ts
+++ client/src/app/front-page/front-page-image/register-club/training-hours-to/training-hours-to.component.ts
@@ -13,6 +13,7 @@ export class TrainingHoursToComponent implements OnInit {
   @Input() matcher: ErrorStateMatcher;
 
   trainingHours: any[] = [
+    {value: 'Rest'},
     {value: '00:00'}, {value: '00:15'}, {value: '00:30'}, {value: '00:45'},
     {value: '01:00'}, {value: '01:15'}, {value: '01:30'}, {value: '01:45'},
     {value: '02:00'}, {value: '02:15'}, {value: '02:30'}, {value: '02:45'},
