1d5721df2b675cef831d38ffa67bf2833cc377b2
mlung16 <martin.l.lundgren@gmail.com>
2019-04-26 16:44:02 +0200

    player db connection

--- Api/BusinessLogic/PlayerLogic.cs
+++ Api/BusinessLogic/PlayerLogic.cs
@@ -1,9 +1,43 @@
-﻿using System;
+﻿using Api.DAL.Entities;
+using Api.DAL.Repos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Api.BusinessLogic {
-    public class PlayerLogic {
+
+    public interface IPlayerService {
+
+        Player Create(Player entity);
+    }
+
+    public class PlayerLogic : IPlayerService {
+        private readonly PlayerRepos _playerRepository;
+        private Account _account;
+
+        public PlayerLogic(PlayerRepos playerRepository) {
+            _playerRepository = playerRepository;
+            _account = new Account();
+        }
+
+        public Player Create(Player entity) {
+
+            //tjek email
+
+            string s = _account.CreatePasswordHash(entity.Password);
+            char[] splitter = { ':' };
+            var split = s.Split(splitter);
+            string salt = split[0];
+            string hashValue = split[1];
+
+            UserCredentials userCredentials = new UserCredentials();
+            userCredentials.HashPassword = hashValue;
+            userCredentials.Salt = salt;
+
+            entity.UserCredentials = userCredentials;
+
+            return _playerRepository.Create(entity);
+        }
     }
 }
--- Api/DAL/Repos/PlayerRepos.cs
+++ Api/DAL/Repos/PlayerRepos.cs
@@ -1,17 +1,90 @@
 ﻿using Api.DAL.Entities;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace Api.DAL.Repos {
     public class PlayerRepos : IRepository<Player> {
-        
-        public Func<IDbConnection> Connection { get; set; }
+
+        private readonly string _connString;
+
+        public PlayerRepos(IConfiguration config) {
+            _connString = config.GetConnectionString("DefaultConnection");
+        }
 
         public Player Create(Player entity) {
-            throw new NotImplementedException();
+
+            Player p = new Player();
+
+            using (var conn = new SqlConnection(_connString)) {
+                conn.Open();
+                
+                using (SqlTransaction tran = conn.BeginTransaction()) {
+                    try {
+
+
+                        //Return usercredentials ID
+                        string userCredentialsSQL = @"INSERT INTO [UserCredentials] ([Hashpassword, Salt]) VALUES (@Hashpassword, @Salt); 
+                                     SELECT CAST(SCOPE_IDENTITY() as int)";
+
+                        var userCredentials_ID = conn.Query<int>(userCredentialsSQL, new { Hashpassword = entity.UserCredentials.HashPassword, Salt = entity.UserCredentials.Salt}).Single();
+
+                       
+                        //Return primary position ID
+                        string primaryPositionSQL = @"Select position_id from position where name = @PrimaryPosition";
+
+                        var primaryPosition_ID = conn.Query<int>(primaryPositionSQL, new { PrimaryPosition = entity.PrimaryPosition }).Single();
+
+
+                        //Return secondary position ID
+                        string secondaryPositionSQL = @"Select position_id from position where name = @SecondaryPosition";
+
+                        var secondaryPosition_ID = conn.Query<int>(secondaryPositionSQL, new { SecondaryPosition = entity.SecondaryPosition }).Single();
+
+                        //Return current club primary position ID
+                        string currentClubPrimaryPositionSQL = @"Select position_id from position where name = @CurrentClubPrimaryPosition";
+
+                        var currentClubPrimaryPosition_ID = conn.Query<int>(currentClubPrimaryPositionSQL, new { CurrentClubPrimaryPosition  = entity.CurrentClubPrimaryPosition }).Single();
+
+                        //Return current club secondary position ID
+                        string currentClubSecondaryPositionSQL = @"Select position_id from position where name = @CurrentClubSecondaryPosition";
+
+                        var currentClubSecondaryPosition_ID = conn.Query<int>(currentClubSecondaryPositionSQL, new { CurrentClubPrimaryPosition = entity.CurrentClubSecondaryPosition }).Single();
+                        
+                        
+                        //Insert Player
+                        string playerSQL = @"INSERT INTO [Player] ([Firstname, Lastname, Email, Day, Month, Year, Country, Height, Weight, Bodyfat, PreferredHand, CurrentClub, StrengthDescription, 
+                                        WeaknessDescription, VideoPath, ImagePath, PrimaryPosition_ID, SecondaryPosition_ID, CurrentClubPrimaryPosition_ID, CurrentClubPrimaryPosition_ID, UserCredentials_ID]) 
+                                        VALUES (@Firstname, @Lastname, @Email, @Day, @Month, @Year, @Country, @Height, @Weight, @Bodyfat, @PreferredHand, @CurrentClub, @StrengthDescription, @WeaknessDescription,
+                                        @VideoPath, @ImagePath, @PrimaryPosition_ID, @SecondaryPosition_ID, @CurrentClubPrimaryPosition_ID, @CurrentClubSecondaryPosition_ID, @UserCredentials_ID)";
+
+                        var rowCount = conn.Execute(playerSQL, new { Firstname = entity.FirstName, Lastname = entity.LastName, entity.Email, entity.Day, entity.Month, entity.Year, entity.Country, entity.Height, entity.Weight,
+                                                      entity.BodyFat, entity.PreferredHand, entity.CurrentClub, entity.StrengthDescription, entity.WeaknessDescription, entity.VideoPath, entity.ImagePath,
+                                                      PrimaryPosition = primaryPosition_ID, SecondaryPosition = secondaryPosition_ID, CurrentClubPrimaryPosition = currentClubPrimaryPosition_ID, CurrentClubSecondaryPosition = currentClubSecondaryPosition_ID, UserCredentials_ID = userCredentials_ID}, tran);
+
+                        
+                        if (rowCount == 0) {
+                            p.ErrorMessage = "The player was not registred";
+                            tran.Rollback();
+                        }
+                        else {
+                            p.ErrorMessage = "";
+                            tran.Commit();
+                        }
+                    }
+                    catch (SqlException e) {
+
+                        tran.Rollback();
+                        p.ErrorMessage = ErrorHandling.Exception(e);                 
+                    }
+                }
+            }
+            return p;
         }
 
         public int Delete(int id) {
