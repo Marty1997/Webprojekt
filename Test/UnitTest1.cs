739cd8fe60fc4056656b1687ae6c8c3824a624b5
Morten Albertsen <1067316@ucn.dk>
2019-05-13 11:07:21 +0200

    fIX TIL RUNE

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+using System;
+using Xunit;
+using Api;
+
+namespace Test {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+            Assert("9asdjklwedjnjnoed9");
+        }
+    }
+}
