07fe7abfa6f286c7d7c217c462e5d540cc8df5dc
Sunday Kenyi Lemi <1067865@ucn.dk>
2019-05-16 10:03:49 +0200

    Backend + frontend funktionalitet--den rigtige

--- client/src/app/multi-page/contact-adviser/contact-adviser.component.ts
+++ client/src/app/multi-page/contact-adviser/contact-adviser.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import { EmailService } from 'src/app/services/emailService';
+import { loginService } from 'src/app/services/loginService';
+import { Player } from 'src/app/models/player.model';
+import { Club } from 'src/app/models/club.model';
 
 @Component({
   selector: 'app-contact-adviser',
@@ -13,14 +16,26 @@ export class ContactAdviserComponent implements OnInit {
 
   isCollapsed: boolean = true;
 
+  playerBinding: Player;
+  clubBinding: Club;
+
   form: FormGroup;
   email: FormControl;
+  message: FormControl;
 
-  constructor(private emailService: EmailService) { }
+  constructor(private emailService: EmailService, private loginService: loginService) { }
 
   ngOnInit() {
     this.createFormControls();
     this.createForm();
+
+    if (this.loginService.typeOfLogin == "Player") {
+      this.playerBinding = this.loginService.playerInSession;
+    }
+
+    if (this.loginService.typeOfLogin == "Club") {
+      this.clubBinding = this.loginService.clubInSession;
+    }
   }
 
   createFormControls() {
@@ -28,28 +43,33 @@ export class ContactAdviserComponent implements OnInit {
       Validators.required,
       Validators.pattern("[^ @]*@[^ @]*")
     ]);
+
+    this.message = new FormControl();
   }
 
   createForm() {
     this.form = new FormGroup({
       email: this.email,
+      message: this.message,
     });
   }
 
-  onSubmit() {
-    if (this.form.valid) {
-      console.log("Form Submitted!");
-      console.log(this.form);
-      //this.emailService.sendContactEmail(this.form);
-      this.form.reset();
-    }
-    else{
-      console.log("Form not Submitted!");
-    }
+  onSubmit(form: NgForm) {
+    // if (this.form.valid) {
+    //   console.log("Form Submitted!");
+    //   console.log(this.form);
+    //   console.log(this.form.value);
+    //   this.emailService.sendContactEmail(this.form);
+    //   this.form.reset();
+    // }
+    // else{
+    //   console.log("Form not Submitted!");
+    // }
+    console.log(this.form)
   }
 
-  toggle(){
+  toggle() {
     this.isCollapsed = !this.isCollapsed;
   }
-
+  
 }
--- client/src/app/services/emailService.ts
+++ client/src/app/services/emailService.ts
@@ -6,12 +6,21 @@ export class EmailService {
     constructor(private http: HttpClient) {
     }
 
-    sendContactEmail() {
+    sendContactEmail(form: any) {
         let url = "https://localhost:44310/api/email/"
         const body = {
-            email: "lkasmd",
-            message: "lkmasdlkmasdlmk"
-        }
-        return this.http.post(url, body);
+            email: form.value.email,
+            message: form.value.message,
+        };
+        return this.http.post(url, body).subscribe(
+            (success) => {
+                console.log("Message sent // Besked sendt")
+                return true;
+            },
+            (error) => {
+                console.error("failed to send // besked ikke sendt");
+                return false;
+            }
+        );
     }
 }
