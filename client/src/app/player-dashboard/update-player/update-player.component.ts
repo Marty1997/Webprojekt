7e24336dba3d6528c0f7df163f540f7a44b5aada
Rune Baun Andersen <1067514@ucn.dk>
2019-06-03 10:12:16 +0200

    update player info

--- client/src/app/player-dashboard/update-player/update-player.component.ts
+++ client/src/app/player-dashboard/update-player/update-player.component.ts
@@ -4,6 +4,8 @@ import { loginService } from "src/app/services/loginService";
 import { updateService } from "src/app/services/updateService";
 import { deleteService } from "src/app/services/deleteService";
 import { uploadFilesService} from "src/app/services/uploadFilesService";
+import { FormControl, Validators } from '@angular/forms';
+import { MyErrorStateMatcher } from 'src/app/front-page/front-page-image/register-player/register-player.component';
 
 @Component({
   selector: 'app-update-player',
@@ -14,6 +16,67 @@ export class UpdatePlayerComponent implements OnInit {
 
   playerBinding: Player;
   step: number;
+
+  // Validators
+  validate = new MyErrorStateMatcher();
+  numbersOnlyRegex = /^[0-9]*$/;
+  currentPassword = new FormControl("", [
+    Validators.required,
+    Validators.minLength(6)
+  ]);
+  password = new FormControl("", [
+    Validators.required,
+    Validators.minLength(6)
+  ]); 
+  aTeamNumberControl = new FormControl(
+    "",
+    Validators.pattern(this.numbersOnlyRegex)
+  );
+  bTeamNumberControl = new FormControl(
+    "",
+    Validators.pattern(this.numbersOnlyRegex)
+  );
+  u21NumberControl = new FormControl(
+    "",
+    Validators.pattern(this.numbersOnlyRegex)
+  );
+  u18NumberControl = new FormControl(
+    "",
+    Validators.pattern(this.numbersOnlyRegex)
+  );
+  emailControl = new FormControl("", [Validators.required, Validators.email]);
+  passwordControl = new FormControl("", [
+    Validators.required,
+    Validators.minLength(6)
+  ]);
+  firstNameControl = new FormControl("", Validators.required);
+  lastNameControl = new FormControl("", Validators.required);
+  countryControl = new FormControl("", Validators.required);
+  dayControl = new FormControl("", [
+    Validators.required,
+    Validators.minLength(2),
+    Validators.maxLength(2),
+    Validators.pattern(this.numbersOnlyRegex)
+  ]);
+  monthControl = new FormControl("", Validators.required);
+  yearControl = new FormControl("", [
+    Validators.required,
+    Validators.minLength(4),
+    Validators.maxLength(4),
+    Validators.pattern(this.numbersOnlyRegex)
+  ]);
+  heightControl = new FormControl(
+    "",
+    Validators.pattern(this.numbersOnlyRegex)
+  );
+  weightControl = new FormControl(
+    "",
+    Validators.pattern(this.numbersOnlyRegex)
+  );
+  bodyfatControl = new FormControl(
+    "",
+    Validators.pattern(this.numbersOnlyRegex)
+  );
 
   constructor(
     private loginService: loginService,
@@ -92,5 +155,219 @@ export class UpdatePlayerComponent implements OnInit {
   cancel() {
     this.playerBinding = this.loginService.playerInSession;
   }
+
+  countryList: string[] = [
+    "Denmark",
+    "Norway",
+    "Sweden",
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antigua &amp; Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bermuda",
+    "Bhutan",
+    "Bolivia",
+    "Bosnia &amp; Herzegovina",
+    "Botswana",
+    "Brazil",
+    "British Virgin Islands",
+    "Brunei",
+    "Bulgaria",
+    "Burkina Faso",
+    "Burundi",
+    "Cambodia",
+    "Cameroon",
+    "Canada",
+    "Cape Verde",
+    "Cayman Islands",
+    "Chad",
+    "Chile",
+    "China",
+    "Colombia",
+    "Congo",
+    "Cook Islands",
+    "Costa Rica",
+    "Cote D Ivoire",
+    "Croatia",
+    "Cruise Ship",
+    "Cuba",
+    "Cyprus",
+    "Czech Republic",
+    "Denmark",
+    "Djibouti",
+    "Dominica",
+    "Dominican Republic",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "Equatorial Guinea",
+    "Estonia",
+    "Ethiopia",
+    "Falkland Islands",
+    "Faroe Islands",
+    "Fiji",
+    "Finland",
+    "France",
+    "French Polynesia",
+    "French West Indies",
+    "Gabon",
+    "Gambia",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Gibraltar",
+    "Greece",
+    "Greenland",
+    "Grenada",
+    "Guam",
+    "Guatemala",
+    "Guernsey",
+    "Guinea",
+    "Guinea Bissau",
+    "Guyana",
+    "Haiti",
+    "Honduras",
+    "Hong Kong",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Iran",
+    "Iraq",
+    "Ireland",
+    "Isle of Man",
+    "Israel",
+    "Italy",
+    "Jamaica",
+    "Japan",
+    "Jersey",
+    "Jordan",
+    "Kazakhstan",
+    "Kenya",
+    "Kuwait",
+    "Kyrgyz Republic",
+    "Laos",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libya",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg",
+    "Macau",
+    "Macedonia",
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Mauritania",
+    "Mauritius",
+    "Mexico",
+    "Moldova",
+    "Monaco",
+    "Mongolia",
+    "Montenegro",
+    "Montserrat",
+    "Morocco",
+    "Mozambique",
+    "Namibia",
+    "Nepal",
+    "Netherlands",
+    "Netherlands Antilles",
+    "New Caledonia",
+    "New Zealand",
+    "Nicaragua",
+    "Niger",
+    "Nigeria",
+    "Norway",
+    "Oman",
+    "Pakistan",
+    "Palestine",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines",
+    "Poland",
+    "Portugal",
+    "Puerto Rico",
+    "Qatar",
+    "Reunion",
+    "Romania",
+    "Russia",
+    "Rwanda",
+    "Saint Pierre &amp; Miquelon",
+    "Samoa",
+    "San Marino",
+    "Satellite",
+    "Saudi Arabia",
+    "Senegal",
+    "Serbia",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Slovakia",
+    "Slovenia",
+    "South Africa",
+    "South Korea",
+    "Spain",
+    "Sri Lanka",
+    "St Kitts &amp; Nevis",
+    "St Lucia",
+    "St Vincent",
+    "St. Lucia",
+    "Sudan",
+    "Suriname",
+    "Swaziland",
+    "Sweden",
+    "Switzerland",
+    "Syria",
+    "Taiwan",
+    "Tajikistan",
+    "Tanzania",
+    "Thailand",
+    "Timor L'Este",
+    "Togo",
+    "Tonga",
+    "Trinidad &amp; Tobago",
+    "Tunisia",
+    "Turkey",
+    "Turkmenistan",
+    "Turks &amp; Caicos",
+    "Uganda",
+    "Ukraine",
+    "United Arab Emirates",
+    "United Kingdom",
+    "United States",
+    "United States Minor Outlying Islands",
+    "Uruguay",
+    "Uzbekistan",
+    "Venezuela",
+    "Vietnam",
+    "Virgin Islands (US)",
+    "Yemen",
+    "Zambia",
+    "Zimbabwe"
+  ];
 }
 
